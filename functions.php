f7733220497313648ae944d0c7730375d188e47c
unknown <Student@digitalschool.local>
2026-01-21 20:22:24 +0100

    New changes

--- functions.php
+++ functions.php
@@ -9,5 +9,36 @@ function load_script() {
 
 add_action('wp_enqueue_scripts','load_script');
 
+function config(){
+
+    register_nav_menus(
+        array(
+            'wp_devs_main_menu' => 'Main Menu',
+            'wp_devs_footer_menu' => 'Footer Menu'
+        ));
+}
+
+$args = array(
+    'width' => 225,
+    'height' => 1920
+);
+
+add_theme_support('custom-header', $args);
+add_theme_support('post-thumbnails');
+add_theme_support('coustom-logo', array(
+    'height' => 110,
+    'width' => 200,
+    'flex-height' => true,
+    'flex-width' =>
+));
+
+
+add_theme_support('automatic-feed-links');
+add_theme_support('html5', array('comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script'));
+add_theme_support('title-tag');
+
+
+add_action('after_setup_theme', 'config', 0);
+
 
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,45 @@
 <?php get_header();  ?>
-<h1>This is our new theme.</h1>
+
+
+<div id="content" class="site-content">
+
+     <div id="primary" class="content-area">
+
+
+       <main id="main" class="site-main">
+        <h1>Blog</h1>
+        <div class="container">
+            <div class="blog-item">
+                
+
+
+                 <?php
+                 if(have_posts()):
+                    while(have_posts()): the_post();
+                    get_template_part('parts/content');
+                 endwhile;
+                 ?>
+
+                 <div class="wpdevs-pagination">
+                    <div class="pages new">
+                        <?php previous_posts_link("<< Newer posts"); ?>
+                    </div>
+
+                    <div class="pages old">
+                        <?php previous_posts_link("<< Older posts"); ?>
+                    </div>
+                 </div>
+                 <?php
+                 else:
+                 ?>
+                 <p>Nothing yet to be displayed</p>
+                 <?php endif; ?>
+            </div>
+            <?php get_sidebar(); ?>
+        </div>
+       </main>
+     </div>
+</div>
+
+
 <?php get_footer();  ?>
