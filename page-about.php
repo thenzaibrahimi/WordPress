8c3d022ec3b8d8b69823e5f5c062d3707e04c3e3
unknown <Student@DIGITALSCHOOL.LOCAL>
2026-01-26 20:25:55 +0100

    Templates

---
+++ page-about.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+
+Template Name: About Page
+
+*/
+?>
+
+<?php  get_header();  ?>
+
+<div id="content" class="site-content">
+    <main id="main" class="site-main">
+        <section class="about-hero">
+            <div class="container">
+                <h1>About us</h1>
+                <p>Welcome to our tech corner - where innovation meets explanation.</p>
+            </div>
+        </section>
+       
+        <section class="about-intro">
+            <div class="container">
+                <h2>Who We Are</h2>
+                <p>We are a team of developers, engineers and tech enthusiasts who love to write about technology.</p>
+            </div>
+        </section>
+
+        <section class="about-mission">
+            <div class="container">
+                <h2>Our Mission</h2>
+                <p>Our goal is simple: to empower curious minds and passionate techies by delicerins accurate, useful and accesible content.</p>
+            </div>
+        </section>
+
+         <section class="about-what-we-cover">
+            <div class="container">
+                <h2>What We Cover</h2>
+                <ul>
+                    <li>Programming tutorials and tips</li>
+                    <li>Software and app reviews</li>
+                    <li>Web development, DevOps and Cloud computing</li>
+                    <li>Trends in tech and industry news.</li>
+                </ul>
+            </div>
+         </section>
+
+         <section class="about-contact">
+            <div class="container">
+                <h2>Want to Connect</h2>
+                <p>We love hearing from our readers! Reach out to us via our Contact page or follow us on social media to stay updated</p>
+            </div>
+         </section>
+    </main>
+</div>
+
+<?php get_footer(); ?>
