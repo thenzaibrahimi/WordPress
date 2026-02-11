66b6ad0f8757ac9a72260f66f585dad4f3d671da
unknown <Student@DIGITALSCHOOL.LOCAL>
2026-02-11 20:25:56 +0100

    Style Css

--- header.php
+++ header.php
@@ -5,8 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <?php
-    wp_head();
+       wp_head();
     ?>
 </head>
-<body>
-   
+<body <?php body_class(array('ds-theme')); ?>>
+
+
+  <?php
+ 
+ if(!is_page('landing-page')):?>
+
+
+  <section class="menu-area">
+
+
+     <div class="container">
+
+
+         <nav class="main-menu">
+             <button class="check-button">
+                 <div class="menu-icon">
+                     <div class="bar1"></div>
+                      <div class="bar2"></div>
+                       <div class="bar3"></div>
+                 </div>
+             </button>
+             <?php wp_nav_menu(array('theme_location' => 'wp_devs_main_menu', 'depth' => 2));  ?>
+         </nav>
+     </div>
+  </section>
+
+
+<?php endif;  ?>
